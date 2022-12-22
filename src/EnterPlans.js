0d17517d58552d04737de2953f175436384c01b9
DelroyGayle <delroygayle01@gmail.com>
2022-12-22 19:01:49 +0000

    update before meeting

--- src/EnterPlans.js
+++ src/EnterPlans.js
@@ -38,14 +38,30 @@ const monthNames = [
   "Nov",
   "Dec",
 ];
+
+/* EG FOR new Date("2022-03-01" 
+filenameTimeStamp => USERNAME:20220201:000000 - This is used as the unique key for the Plan SQL records 
+displayTimeStamp => 01 Mar 2022 00:00:00  - shown onscreen
+*/
+
 const currentTimeStamp = new Date(); // Current Date and Time
-const filenameTimeStamp = Date.now();
 const dayNumber = currentTimeStamp.getUTCDate(); // Day
 // getUTCMonth() is zero-based value (where zero indicates the first month of the year).
 // So January is ZERO
 const monthNumber = currentTimeStamp.getUTCMonth(); // Month
-const yearNumber = currentTimeStamp.getFullYear(); // Year
-let displayTimeStamp = `${dayNumber} ${monthNames[monthNumber]} ${yearNumber}`;
+const yearNumber = currentTimeStamp.getUTCFullYear(); // Year
+const hoursNumber = currentTimeStamp.getUTCHours(); // Hours
+const minutesNumber = currentTimeStamp.getUTCMinutes(); // Minutes
+const secondsNumber = currentTimeStamp.getUTCSeconds(); // Second
+const filenameTimeStamp =
+  `USERNAME:${yearNumber}${String(monthNumber).padStart(2, "0")}${String(dayNumber).padStart(2, "0")}:` +
+  `${String(hoursNumber).padStart(2, "0")}${String(minutesNumber).padStart(2,"0")}` +
+  `${String(secondsNumber).padStart(2, "0")}`;
+const displayTimeStamp = `${String(dayNumber).padStart(2, "0")} ${monthNames[monthNumber]} ${yearNumber}` +
+                         ` ${String(hoursNumber).padStart(2, "0")}:${String(minutesNumber).padStart(2,"0")}:` +
+                         `${String(secondsNumber).padStart(2, "0")}`;
+  
+// EG USERNAME:20221122:184715
 const EnterPlans = () => {
   //   const search = (searchVal) => {
   //     console.info("TO DO!", searchVal);
@@ -280,9 +296,9 @@ const EnterPlans = () => {
 
   return (
     <div>
-      <header className="user-header">
+      <header className="display-flex">
         <div className="title-header">MY SMART GOALS</div>
-        <div className="title-header">{displayTimeStamp}</div>
+        <div className="title-header timestamp-header">{displayTimeStamp}</div>
       </header>
       <table className="table">
         <tr>
